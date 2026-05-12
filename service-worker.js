cd04a340bbaeceb5913beb51f3970df43a4f4c93
JeZach <jens@zachariassen.se>
2026-05-12 21:50:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "privatbudget-github-v3";
+const CACHE_NAME = "privatbudget-github-v4";
 const ASSETS = ["./", "./index.html", "./config.js", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
